c18a002d7f86d9f745b8729c5c1f6657770e3abe
Mallika Parulekar <mallika.parulekar@gmail.com>
2021-10-23 00:19:42 -0700

    text message without security risk

--- calmemaybe/send_sms.py
+++ calmemaybe/send_sms.py
@@ -7,8 +7,7 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-# account_sid = os.environ['AC9812c4a0456958d25531f382f6320f81']
-# auth_token = os.environ['83ddb455f7e1c47d5591ab4ae1c0e848']
+
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
